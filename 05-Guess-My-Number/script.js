6f23a491685afc239f97daba725cbf13e9218464
Amrit Poudel <amritpoudel433@gmail.com>
2021-12-09 22:00:30 +0545

    Emplementing again button

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -15,35 +15,96 @@
   */
 
 
-      const secertnumber = Math.trunc(Math.random()*20)+1 ;
+     let secertnumber = Math.trunc(Math.random()*20)+1 ;
 
       let score = 20 ;
 
 
-      document.querySelector('.number').textContent = secertnumber ;
+     
     document.querySelector('.check').addEventListener('click' , function () {
         const guess =Number( document.querySelector('.guess').value); 
         console.log( guess);
 
- 
+   // Input is wrong
          if (!guess) {
              document.querySelector('.message').textContent = '⛔ No Number ';
              
-         } else if ( guess === secertnumber){
+         }
+         
+         // correct guess 
+
+         else if ( guess === secertnumber){
              document.querySelector('.message').textContent = ' 🎉🎉 Correct Nunmber...'
 
              document.querySelector('.label-highscore').textContent = score ;
-         } else if ( guess > secertnumber){
 
-            document.querySelector('.message').textContent = ' 📈Too High...'
-  score -- ;
-   document.querySelector('.score').textContent = score ;
+             document.querySelector('.number').textContent = secertnumber ;
+
+
+             document.querySelector('body').style.backgroundColor = '#60b347'
+
+             document.querySelector('.number').style.width = '30rem';
          }
+         
+          // input is too high 
+
+         else if ( guess > secertnumber){
+
+               
+             if (score > 1) {
+                document.querySelector('.message').textContent = ' 📈Too High...'
+                score -- ;
+                 document.querySelector('.score').textContent = score ;
+             } else {
+                 document.querySelector('.message').textContent = ' 🥴You lost the game '
+                 document.querySelector('.score').textContent = 0 ;
+             };
+           
+         }
+
+  // input is too low..
+
+
          else if ( guess < secertnumber){
-            document.querySelector('.message').textContent = ' 📉 Too Low...'
-            score -- ;
-            document.querySelector('.score').textContent = score ;
+            if (score > 1) {
+                document.querySelector('.message').textContent = ' 📈Too Low...'
+                score -- ;
+                 document.querySelector('.score').textContent = score ;
+             } else {
+                 document.querySelector('.message').textContent = ' 🥴You lost the game '
+                 document.querySelector('.score').textContent = 0 ;
+             };
+           
          }
 
 
     });
+
+      // For clicking again
+
+    document.querySelector('.again').addEventListener('click' , function () {
+
+        secertnumber = Math.trunc(Math.random()*20)+1 ;
+
+
+      document.querySelector('.score').textContent = 20 ;
+
+      document.querySelector('body').style.backgroundColor = '#222' ;
+
+      document.querySelector('.number').style.width = '15rem';
+
+document.querySelector('.message').textContent = 'start quessing ...' ;
+
+     document.querySelector('.number').textContent = '?'
+
+     document.querySelector('.guess').value  = ''
+       
+    });
+
+
+
+
+
+
+
+
