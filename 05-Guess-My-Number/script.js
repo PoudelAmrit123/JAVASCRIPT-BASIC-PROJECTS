9e2ce1442da9cc6747c77537556b03dc6ebd18d4
Amrit Poudel <amritpoudel433@gmail.com>
2021-12-09 20:22:00 +0545

    basic of dom manupulation

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,17 @@
+
 'use strict';
+
+  // Dom manupulation basic 
+  /*
+    console.log( document.querySelector('.message').textContent) ;
+   
+
+    document.querySelector('.number').textContent = 13 ;
+
+    document.querySelector('.guess').value = 25 ;
+    
+    console.log( document.querySelector('.score').textContent);
+
+  */
+
+    
