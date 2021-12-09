6da6465eedc41068c70e096d2dc2bd652e423114
Amrit Poudel <amritpoudel433@gmail.com>
2021-12-09 21:08:29 +0545

    Doing high and low value

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -14,4 +14,36 @@
 
   */
 
-    
+
+      const secertnumber = Math.trunc(Math.random()*20)+1 ;
+
+      let score = 20 ;
+
+
+      document.querySelector('.number').textContent = secertnumber ;
+    document.querySelector('.check').addEventListener('click' , function () {
+        const guess =Number( document.querySelector('.guess').value); 
+        console.log( guess);
+
+ 
+         if (!guess) {
+             document.querySelector('.message').textContent = '⛔ No Number ';
+             
+         } else if ( guess === secertnumber){
+             document.querySelector('.message').textContent = ' 🎉🎉 Correct Nunmber...'
+
+             document.querySelector('.label-highscore').textContent = score ;
+         } else if ( guess > secertnumber){
+
+            document.querySelector('.message').textContent = ' 📈Too High...'
+  score -- ;
+   document.querySelector('.score').textContent = score ;
+         }
+         else if ( guess < secertnumber){
+            document.querySelector('.message').textContent = ' 📉 Too Low...'
+            score -- ;
+            document.querySelector('.score').textContent = score ;
+         }
+
+
+    });
