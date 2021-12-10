97b8d5ef39edcf76de73c90c570cd18b5688a470
Amrit Poudel <amritpoudel433@gmail.com>
2021-12-10 20:22:34 +0545

    targeting the class

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+ const modal = document.querySelector('.modal');
+ const overlay = document.querySelector('.overlay');
+
+ const btnclose = document.querySelector('.close-modal');
+
+ const btnsopen = document.querySelectorAll('.show-modal');
+
+  console.log(btnsopen);
+
+
+   for ( let i=0 ; i< btnsopen.length ; i++){
+      (btnsopen[i].addEventListener('click' , function(){
+
+       
+   console.log('button clicked');
+        // modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+
+    document.querySelector('.modal').classList.remove('hidden');
+
+   
+      }));
+
+  
+        btnclose.addEventListener('click' , function(){
+
+   modal.classList.add('hidden');
+   overlay.classList.add('hidden');
+
+        })
+
+
+   }
+
+   
+
+
+
+
+
