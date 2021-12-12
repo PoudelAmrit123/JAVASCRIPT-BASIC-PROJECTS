ae7d8680999cbedf300b796204ea3c41e37fe1e4
Amrit Poudel <amritpoudel433@gmail.com>
2021-12-12 16:27:09 +0545

    selecting the element and starting condition

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+ // selecting the element
+const score0 = document.querySelector('#score--0');
+
+const score1 = document.getElementById('score--1');
+
+ const dice = document.querySelector('.dice');
+  
+// starting condition
+
+  score0.textContent = 0;
+  score1.textContent = 0;
+  dice.classList.add('hidden');
+
+  
+  
+  
