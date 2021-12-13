00ddbc445f5013d54c2c54f706233c88e2625c81
Amrit Poudel <amritpoudel433@gmail.com>
2021-12-13 14:28:51 +0545

    holding current

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -25,12 +25,16 @@ const current1 = document.querySelector('#current--1') ;
   score1.textContent = 0;
   dice.classList.add('hidden');
 
+
+    const scores = [ 0 , 0];
 let activeplayer = 0;
     let currentscore =0 ;
+  let playing = true ;
 
   
   btnroll.addEventListener('click' , function(){
 
+      if ( playing){
    const dicen = Math.trunc(Math.random()*6)+1;
   console.log(dicen);
    dice.classList.remove('hidden');
@@ -64,4 +68,68 @@ activeplayer = activeplayer === 0 ? 1 : 0 ;
   }
    
 
-  })
+    }  })
+
+
+btnhold.addEventListener('click' , function(){
+  if ( playing){
+      
+scores[activeplayer] += currentscore ; 
+
+ document.getElementById(`score--${activeplayer}`).textContent = scores[activeplayer] ;
+
+      if ( scores[activeplayer ] >= 20){
+
+          document.querySelector(`.player--${activeplayer}`).classList.add('player--winner')
+
+
+
+          document.querySelector(`.player--${activeplayer}`).classList.remove('player--active');
+
+
+ playing = false ;
+
+
+
+
+       }  else {
+activeplayer = activeplayer === 0 ? 1 : 0 ;
+
+      currentscore = 0;
+      
+    document.getElementById(`current--${activeplayer}`).textContent = currentscore;
+
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');  
+
+      }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
