659733092f91c1289bd1b7d533f066093de6fe5b
Amrit Poudel <amritpoudel433@gmail.com>
2021-12-22 23:17:23 +0545

    String Practise

--- Data Structures, Modern Operators and Strings/starter/script.js
+++ Data Structures, Modern Operators and Strings/starter/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// `  `
+// 
 
    
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 
@@ -99,7 +99,7 @@ const restaurant = {
 
   // STIRNG 
 
-
+ /*
   const airline = 'Tribhuvan Internatinal' ;
         
 
@@ -251,16 +251,29 @@ maskcredit(6643614868478);
 
 
 
+  */
+
+
+
+       //Coding Challenge 4 String adn practise
+
+       const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 
 
 
-       //Coding Challenge 4 String
+  for ( const flight of flights.split('+')){
+  const [ where , from , to , time] = flight.split(';');
+
+  const output = ` ${where.startsWith('_Delayed') ? '🚫' : ' '} ${where.replaceAll('_' , ' ')} from ${from.slice(0 , 3).toUpperCase()} to ${to.slice(0 , 3).toUpperCase() } (${time.replace(':' , 'h')}) `.padStart(40);
+
+  console.log(output);
+
+  }
 
 
 
-
-       
 
 
 
