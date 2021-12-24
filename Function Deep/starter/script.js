41bd97f023a657f3952f5d85d1ad17337b7d0185
Amrit Poudel <amritpoudel433@gmail.com>
2021-12-24 22:03:01 +0545

    Default parameter

--- Function Deep/starter/script.js
+++ Function Deep/starter/script.js
@@ -1,7 +1,27 @@
 'use strict';
 
 
-   // Moving from the basic introduction to some more knowledge of the function
+   // Moving from the basic introduction to some more Function
 
-   const happy = 'Awesome'
-  console.log(` Function is  ${happy} ! 😊`);
+  //Default parameter
+
+
+
+  const bookings = [];
+   const createBooking = function  (FlightNum , Noofpassenger=1 , price = 199 *Noofpassenger) {
+// here in Es6 we can set default value right away in the function directly and can also set the value which is predefined like that of passenger 
+
+      const booking = {
+         FlightNum ,
+         Noofpassenger , 
+         price ,
+      };
+      console.log(booking);
+      bookings.push(booking);
+   }
+   createBooking('Lh20'  )
+   createBooking('Lh20' , 2 , 1200 )
+   createBooking('Lh20' , undefined , 1200 ) //If we want that default value but as we cannot skip the aargument 
+   
+   
+   console.log(bookings);
