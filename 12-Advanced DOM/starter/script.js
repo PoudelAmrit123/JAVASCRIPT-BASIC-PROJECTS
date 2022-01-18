803b5b0d0bdb561db7ba04c225e36c72f67352a1
Amrit Poudel <amritpoudel433@gmail.com>
2022-01-18 11:53:50 +0545

    Dom Tranversing

--- 12-Advanced DOM/starter/script.js
+++ 12-Advanced DOM/starter/script.js
@@ -92,6 +92,15 @@ document.addEventListener('keydown', function (e) {
 
 
 
+
+
+
+
+
+
+
+
+
     // Lecture *****
     // Selecting elements
 
@@ -188,16 +197,63 @@ logo.clasName = 'jonas';
 
 ///////////////////////////////////////
 // Types of Events and Event Handlers
+ /*
+
 const h1 = document.querySelector('h1');
 
 const alertH1 = function (e) {
   alert('addEventListener: Great! You are reading the heading :D');
 };
-
+  
 h1.addEventListener('mouseenter', alertH1);
 
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
+
+  */
+ 
 // h1.onmouseenter = function (e) {
 //   alert('onmouseenter: Great! You are reading the heading :D');
 // };
+
+
+
+
+///////////////////////////////////////
+// DOM Traversing
+
+
+  /*
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
+
+ */
+
+
