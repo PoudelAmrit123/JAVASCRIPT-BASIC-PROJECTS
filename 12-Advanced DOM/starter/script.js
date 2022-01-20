b43d52c9230263691cf79248a6d361615620026f
Amrit Poudel <amritpoudel433@gmail.com>
2022-01-20 23:47:49 +0545

    Reveal Section

--- 12-Advanced DOM/starter/script.js
+++ 12-Advanced DOM/starter/script.js
@@ -175,8 +175,32 @@ tabsContainer.addEventListener('click', function (e) {
   headerObserver.observe(header);
   
 
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+   
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);// NOt Nedded 
+  
+};
 
 
+  
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 
 
