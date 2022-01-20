ec3b9ada19382aa0cb8a4516d20b4d5326fb55ec
Amrit Poudel <amritpoudel433@gmail.com>
2022-01-21 00:32:35 +0545

    Lazy img loading

--- 12-Advanced DOM/starter/script.js
+++ 12-Advanced DOM/starter/script.js
@@ -186,7 +186,7 @@ const revealSection = function (entries, observer) {
 
   entry.target.classList.remove('section--hidden');
   observer.unobserve(entry.target);// NOt Nedded 
-  
+
 };
 
 
@@ -201,6 +201,35 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+  
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
+
+
 
 
 
