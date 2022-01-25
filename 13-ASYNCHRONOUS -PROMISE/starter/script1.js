129ac5d5457cdb038d1a8ca67175da1094de3904
Amrit Poudel <amritpoudel433@gmail.com>
2022-01-25 18:15:44 +0545

    call back hell

---
+++ 13-ASYNCHRONOUS -PROMISE/starter/script1.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+//  const renderCountry = function (className ) {
+//          const request = new XMLHttpRequest();
+//          request.open('GET' , 'https://restcountries.com/v3.1/name/${className}');
+
+//   request.send();
+
+//      request.addEventListener('load' , function(){
+
+//      const  data  = JSON.parse(request.responseText);
+
+//          const html = `
+//          <article class="country ${className}">
+//          <img class="country__img" src="${data.flag}" />
+//          <div class="country__data">
+//          <h3 class="country__name">${data.name}</h3>
+//          <h4 class="country__region">${data.region}</h4>
+//            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//          <p class="country__row"><span>👫</span>${(
+//              +data.population / 1000000
+//              ).toFixed(1)} people</p>
+
+//              </div>
+//              </article>
+//              `;
+//              countriesContainer.insertAdjacentHTML('beforeend', html);
+//              countriesContainer.style.opacity = 1;
+//             })
+//             };
+
+//             renderCountry('usa');
+
+
+
+
+             // CALL back hell
+setTimeout(() => {
+  console.log('1 sec has passed');
+
+  setTimeout(() => {
+    console.log('2 sec has passed');
+    setTimeout(() => {
+      console.log('3 sec has passed');
+
+      setTimeout(() => {
+        console.log('4 sec has passed');
+        setTimeout(() => {
+          console.log('5 sec has passed');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+
+// Our First AJAX Call: XMLHttpRequest
+  
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(request.responseText);
+
+    console.log('cool');
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('germany');
